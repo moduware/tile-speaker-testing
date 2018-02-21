a1cec0c65fcf7b7557f9e88571304e81e1668ca8
Alex Chernov <alexusblack@gmail.com>
2018-02-21 11:56:33 +1100

    UI with all functions

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -22,18 +22,21 @@ function updateTimer() {
 
 document.addEventListener('NexpaqAPIReady', function(event) {
 	Nexpaq.API.Module.addEventListener('DataReceived', function(event) {
-        // we don't care about data not related to our module
-    if(event.module_uuid != Nexpaq.Arguments[0]) return;
-		if(event.data_source == 'StateChangeResponse' && event.variables.result == 'success') {
+    // we don't care about data not related to our module
+    if(event.moduleUuid != Nexpaq.Arguments[0]) return;
+		if(event.dataSource == 'StateChangeResponse' && event.variables.result == 'success') {
 			checkStatus();
 		}
-		if(event.data_source == 'StatusRequestResponse') {
+		if(event.dataSource == 'DefaultStateChangeRequestResponse' && event.variables.result == 'success') {
+			checkStatus();
+		}
+		if(event.dataSource == 'StatusRequestResponse') {
 			if(event.variables.status == 'connected') {
 				document.getElementById('speakerStatus').textContent = 'On';
 			} else if(event.variables.status == 'disconnected') {
 				document.getElementById('speakerStatus').textContent = 'Off'
 			}
-			if(Nexpaq.Arguments[2] == 'moduware.speaker.module') {
+			if(Nexpaq.Arguments[2] == 'moduware.module.speaker') {
 				if(event.variables.defaultState == 'connected') {
 					document.getElementById('speakerDefaultStatus').textContent = 'On';
 				} else if(event.variables.defaultState == 'disconnected') {
@@ -41,7 +44,10 @@ document.addEventListener('NexpaqAPIReady', function(event) {
 				}
 			}
 		}
-  });
+		if(event.dataSource == 'BluetoothNameRequestResponse') {
+			document.getElementById('speakerBluetoothName').textContent = event.variables.bluetoothName;
+		}
+	});
 
 	checkStatus();
 });
@@ -54,6 +60,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     checkStatus();
   });
 
+	document.getElementById('requestBluetoothName').addEventListener('click', function() {
+		Nexpaq.API.Module.SendCommand(Nexpaq.Arguments[0], 'AskBluetoothName', []);
+	});
+
 	document.getElementById('connect').addEventListener('click', function() {
 		console.log("connecting");
     Nexpaq.API.Module.SendCommand(Nexpaq.Arguments[0], 'Connect', [1]);
@@ -63,6 +73,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	document.getElementById('disconnect').addEventListener('click', function() {
 		console.log("disconnecting");
     Nexpaq.API.Module.SendCommand(Nexpaq.Arguments[0], 'Disconnect', [0]);
+		status_check_timer = 5;
+	});
+
+	document.getElementById('setDefaultStateOn').addEventListener('click', function() {
+    Nexpaq.API.Module.SendCommand(Nexpaq.Arguments[0], 'SetDefaultStateAsOn', []);
+		status_check_timer = 5;
+	});
+
+	document.getElementById('setDefaultStateOff').addEventListener('click', function() {
+    Nexpaq.API.Module.SendCommand(Nexpaq.Arguments[0], 'SetDefaultStateAsOff', []);
 		status_check_timer = 5;
 	});
 
